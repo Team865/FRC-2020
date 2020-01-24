d8d278e4934c1f2136adcb31d220aa63b1a3ebb8
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-23 22:27:29 -0500

    Add vision state algorithm

--- src/main/java/ca/warp7/frc2020/auton/vision/Limelight.java
+++ src/main/java/ca/warp7/frc2020/auton/vision/Limelight.java
@@ -3,7 +3,10 @@ package ca.warp7.frc2020.auton.vision;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Transform2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 
 @SuppressWarnings("unused")
 public class Limelight {
@@ -14,8 +17,6 @@ public class Limelight {
     private NetworkTableEntry tv = table.getEntry("tv");
     private NetworkTableEntry tx = table.getEntry("tx");
     private NetworkTableEntry ty = table.getEntry("ty");
-    private NetworkTableEntry ta = table.getEntry("ta");
-    private NetworkTableEntry ts = table.getEntry("ts");
 
     public static Limelight getInstance() {
         if (instance == null) instance = new Limelight();
@@ -34,11 +35,57 @@ public class Limelight {
         return ty.getDouble(Double.NaN);
     }
 
-    public double getTargetAreaInImage() {
-        return ta.getDouble(0.0);
-    }
 
-    public Rotation2d getSkew() {
-        return Rotation2d.fromDegrees(ts.getDouble(0.0));
+    // the center location of the power ports on the field
+    private static final Pose2d kTargetToField = new Pose2d(0.0, 1.7, Rotation2d.fromDegrees(-180));
+
+    // the centre height of the target
+    // 81.25 in (bottom) and 17 in (height) -> 89.75in = 2.27965 m
+    private static final double kTargetCentreHeight = 2.27965;
+
+    // the height of the camera relative to the carpet in metres todo
+    private static final double kCameraHeight = 0.0;
+
+    // the transform from the camera lens to the centre of rotation of the robot todo
+    private static final Transform2d kCameraToRobot =
+            new Transform2d(new Translation2d(0.0, 0.0), new Rotation2d());
+
+    // the angle that the camera is mounted relative to the horizontal in degrees.
+    // up is positive todo
+    private static final double kCameraMountingAngle = 0.0;
+
+    // the relative height between the camera and the target
+    private static final double kCameraToTargetHeight = kTargetCentreHeight - kCameraHeight;
+
+
+    /**
+     * Create an estimated robot-to-target pose transform, based on
+     * a robot to field estimation (provided by drive train)
+     * http://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+     */
+    public Pose2d getRobotToTarget(Pose2d robotToFieldEstimation) {
+
+        boolean has_target = hasValidTarget();
+
+        if (!has_target) {
+            // return the best guess based on the drive train
+            return kTargetToField.relativeTo(robotToFieldEstimation);
+        }
+
+        double x = getHorizontalOffset();
+        double y = getVerticalOffset();
+        double camera_to_target_dist = kCameraToTargetHeight / Math.tan(y + kCameraMountingAngle);
+
+        Pose2d camera_to_field_estimation = kTargetToField
+                .relativeTo(robotToFieldEstimation.plus(kCameraToRobot));
+        Rotation2d camera_to_target_angle = camera_to_field_estimation
+                .getRotation().plus(kTargetToField.getRotation());
+
+        Rotation2d angle = camera_to_target_angle.plus(Rotation2d.fromDegrees(x));
+        double offset_x = camera_to_target_dist * angle.getCos();
+        double offset_y = camera_to_target_dist * angle.getSin();
+
+        return new Pose2d(new Translation2d(offset_x, offset_y), angle)
+                .relativeTo(new Pose2d().plus(kCameraToRobot));
     }
 }

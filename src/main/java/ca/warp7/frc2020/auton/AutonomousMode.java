25ad9c894d8c382e11c48be612e8eb772fb2416f
Yu Liu <22801773+yuliu2016@users.noreply.github.com>
2020-02-07 18:43:49 -0500

    Remove Flywheel Characterization Command (#7)

--- src/main/java/ca/warp7/frc2020/auton/AutonomousMode.java
+++ src/main/java/ca/warp7/frc2020/auton/AutonomousMode.java
@@ -2,7 +2,6 @@ package ca.warp7.frc2020.auton;
 
 import ca.warp7.frc2020.auton.commands.DriveCharacterizationCommand;
 import ca.warp7.frc2020.auton.commands.DriveTrajectoryCommand;
-import ca.warp7.frc2020.auton.commands.FlywheelCharacterizationCommand;
 import ca.warp7.frc2020.commands.SingleFunctionCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -24,20 +23,9 @@ public class AutonomousMode {
         );
     }
 
-    public static Command flywheelCharacterizationMode() {
-        return new FlywheelCharacterizationCommand();
-    }
-
-    public static Command lowGearCharacterizationMode() {
+    public static Command driveCharacterizationMode() {
         return new SequentialCommandGroup(
                 SingleFunctionCommand.getSetDriveLowGear(),
-                new DriveCharacterizationCommand()
-        );
-    }
-
-    public static Command highGearCharacterizationMode() {
-        return new SequentialCommandGroup(
-                SingleFunctionCommand.getSetDriveHighGear(),
                 new DriveCharacterizationCommand()
         );
     }
